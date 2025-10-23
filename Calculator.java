d317a0cd9e4ef7df7c331d597c1663cc3f8c3905
Mohamed Salah <mohamedelmorgel2001@gmail.com>
2025-10-23 23:08:09 +0300

    multiplication is perfecto!!!

--- Calculator.java
+++ Calculator.java
@@ -2,131 +2,132 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Calculator{
+public class CalculatorV1 {
 
     public double add(Double... operands) {
-       
-       double sum =0;
-  for (double op : operands) {
-       sum += op;}
 
-      return sum;
-  }
+        double sum = 0;
+        for (double op : operands) {
+            sum += op;
+        }
+
+        return sum;
+    }
 
     public double substract(Double... operands) {
-   
+
 //   double [] sum = new double[operands.length];
-for (int i=1;i<operands.length;i++) {
-    operands[0]-= operands[i];
+        for (int i = 1; i < operands.length; i++) {
+            operands[0] -= operands[i];
 
-}
+        }
 
-  return operands[0];
-}
+        return operands[0];
+    }
 
     public double multiply(Double... operands) {
-   
-   double sum =1;
-for (double op : operands) {
-   sum *= op;}
 
-  return sum;
-}
+        double sum = 1;
+        for (double op : operands) {
+            sum *= op;
+        }
+
+        return sum;
+    }
 
     public double division(Double... operands) {
 
 //    double[] sumA =new double[operands.length];
 //    double sum=1;
-    for (int i=1; i < operands.length;  i++) {
-        if(operands[i]==0 || operands[0]==0){ System.out.println("can't divide by zero"); break;}
+        for (int i = 1; i < operands.length; i++) {
+            if (operands[i] == 0 || operands[0] == 0) {
+                System.out.println("can't divide by zero");
+                break;
+            }
 //    if(operands.length==i){sumA[i]=operands[i]; break;}
-        operands[0] /= operands[i];
-    }
+            operands[0] /= operands[i];
+        }
 
 //    for (int i=0; i < sumA.length-1;  i++) {
 ////        if(sumA.length == i){sum=sumA[i-1]/sumA[i]; break;}
 //        sum = sumA[i]/sumA[i+1];
 //    }
-    return  operands[0];
-}
-
-
-
-
-
-public void calculator(double[] data , char[] operation) {
-    int index=0;
-    for (var op : operation){
-        if(op == '*')
-        {
-            data[index] = multiply(data[index],data[index+1]);
-            index++;
-        }
+        return operands[0];
     }
-    index=0;
-    for (var op : operation){
-             if(op == '/')
-        {
 
-            data[index] = division(data[index],data[index+1]);
-            index++;
+
+    public void calculator(List<Double> data, List<Character> operation) {
+       List<Double> sum = new ArrayList<>();
+        for(int i =0 ; i< data.size();i++){
+            if(operation.get(i) == '*'){
+               data.set(i,multiply(data.get(i), data.get(i+1)));
+               data.remove(i+1);
+               operation.remove(i);
+               --i;
+            }
+            else if (operation.get(i) == '=') {
+                System.out.print("sum= ");
+//                for (int j = 0; j < data.size(); j++) {
+//                    System.out.print(data.get(j)+" ");
+//                    System.out.print(operation.get(j)+" ");
+//
+//                    if (j == data.size() - 1) {
+                        System.out.println(data.get(0));
+
+//                    }
+
+
+            }
+            }
+
+
+
+
+
+
         }
-    } 
-    
-    index=0;
-    
-    for (var op : operation){
-        if(op=='+')
-        {
-            data[index] = add(data[index],data[index+1]);
-            index++;
-        }
-    } 
-    index=0;
-    
-    for (var op : operation){
-        if(op=='-')
-        {
-            data[index] = substract(data[index],data[index+1]);
-            index++;
-        }
+
+
     }
-if (operation[index]=='='){
-    System.out.println("sum = " + data[0]);
-}
-    
-}
 
-public static void main(String[] args) {
+
+
+
+    
+
+
+public void main(String[] args) {
 //    int dataSize = 10;
     System.out.println("Welcome to my humble calculator\nDesigened and developed By\\Mohamed Salah ElMorgel\n2025");
 
     List<Double> data= new ArrayList<>();
     List<Character> operations= new ArrayList<>();
-do {
     int i = 1;
+do {
+
 
 
     System.out.print("Enter opernad" + i + ": ");
+    i++;
     Scanner scan = new Scanner(System.in);
     data.add(scan.nextDouble());
     System.out.print("Enter the operation(\" + - * / = \"): ");
     operations.add(scan.next().charAt(0));
-    i++;
+
     if (operations.getLast() == '=') {
         scan.close();
-//            Calculator calculator=new Calculator();
-//            calculator.calculator(data, operations);
+            CalculatorV1 calc= new CalculatorV1();
+            calc.calculator(data, operations);
 
-        System.out.print("Equation: ");
-        for (int j = 0; j < data.size(); j++) {
-            System.out.print(data.get(j)+" ");
-            System.out.print(operations.get(j)+" ");
-
-            if (j == data.size() - 1) {
-                System.out.println();
-            }
-        }
+//        System.out.print("Equation: ");
+//        for (int j = 0; j < data.size(); j++) {
+//            System.out.print(data.get(j)+" ");
+//            System.out.print(operations.get(j)+" ");
+//
+//            if (j == data.size() - 1) {
+//                System.out.println();
+//            }
+//        }
 
 
     }
@@ -138,7 +139,7 @@ do {
     }
 
 
-}
+
 
 
 
