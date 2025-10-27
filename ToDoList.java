497073f88b9a0813b3404b67b8a78d5cb50b3dee
Mohamed Salah <mohamedelmorgel2001@gmail.com>
2025-10-28 01:37:09 +0300

    ToDoList first structure

---
+++ ToDoList.java
@@ -0,0 +1,40 @@
+/*
+🧾 3. To-Do List (Mini CLI App)
+Goal: Learn how to manage dynamic data and interact with users in loops.
+Features:
+1- Add new tasks
+2- View all tasks
+3- Mark tasks as done
+4- Delete tasks
+5- Exit program
+
+Concepts Practiced:
+
+ArrayList, loops, switch, methods, possibly file writing to save data.*/
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ToDoList
+{
+     static int ID = 1;
+     static ArrayList<ArrayList<String>> DB = new ArrayList<>();
+public static void read_Tasks(){}
+public static void add_Task(String description, LocalDate dueDate, Boolean status){}
+public static void view_Tasks(){}
+public static void mark_Task_Done(){}
+public static void delete_Task(){}
+public static void save(){}
+//public static void exit_Task(){}
+
+
+    public static void main(String[] args ){}
+
+
+
+}
+
+
+
+
+
